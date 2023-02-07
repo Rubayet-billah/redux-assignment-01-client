f9edc1a7a897f99cf463438f7eeba622c3c7fc5f
Rubayet-billah <rubayetbillah909@gmail.com>
2023-02-08 00:22:58 +0600

    card added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Blogs from "./Pages/Blogs/Blogs";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Blogs />
+    </div>
+  );
 }
 
 export default App;
